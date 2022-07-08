b3a566cfee3f62f27d71f993a4ef2929afbe2e3b
Kelvinskell <kelvinskelll@gmail.com>
2022-07-08 12:39:14 +0100

    feat: Add news.py file

---
+++ frontend-service/news.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
