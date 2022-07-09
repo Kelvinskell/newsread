86a959cbf3dbe535c3a3e9a944743ccbea62d650
Kelvinskell <kelvinskelll@gmail.com>
2022-07-09 19:56:16 +0100

    feat: Add  secret key

--- login-service/application/__init__.py
+++ login-service/application/__init__.py
@@ -6,7 +6,7 @@ database.create_database()
 database.create_table()
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '08dae760c2488d8a0dca1bfb'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_DB'] = 'newsread'
