d83816612ea9a5afc5b99f5255d423dd2a3d35f7
Kelvinskell <kelvinskelll@gmail.com>
2022-07-09 13:53:18 +0100

    feat: Import database.py

--- login-service/application/__init__.py
+++ login-service/application/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
 from flask import render_template
+from flask_mysqldb import MySQL
+from application import database
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_DB'] = 'newsread'
+mysql = MySQL(app)
+
 from application import routes
+
