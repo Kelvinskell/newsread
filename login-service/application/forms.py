d55d0349c10042be1030b977586c5747b27f577e
Kelvinskell <kelvinskelll@gmail.com>
2022-07-09 20:53:49 +0100

    feat: Improve decoration for register form

--- login-service/application/forms.py
+++ login-service/application/forms.py
@@ -8,4 +8,4 @@ class RegisterForm(FlaskForm):
     email_address = StringField(label='Email Address')
     password1 = PasswordField(label='Password')
     password2 = PasswordField(label='Confirm Password')
-    submit = SubmitField(label='Submit')
+    submit = SubmitField(label='Create Account')
