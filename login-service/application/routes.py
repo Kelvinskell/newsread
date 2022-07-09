abc570200b4d9d01ef5beca65c00fb3b3811e7ad
Kelvinskell <kelvinskelll@gmail.com>
2022-07-09 18:43:03 +0100

    feat: render register.html template

--- login-service/application/routes.py
+++ login-service/application/routes.py
@@ -1,4 +1,5 @@
 from application import app
+from flask import render_template
 from application import mysql
 import MySQLdb.cursors
 from application.forms import RegisterForm
@@ -6,3 +7,4 @@ from application.forms import RegisterForm
 @app.route('/register')
 def register_page():
     form = RegisterForm()
+    return render_template('register.html', form=form)
