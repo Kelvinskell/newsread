bab90c18fcbb106824e95a9295e11690e0603cc0
Kelvinskell <kelvinskelll@gmail.com>
2022-07-10 06:06:25 +0100

    feat: Extend validations to password field

--- login-service/application/forms.py
+++ login-service/application/forms.py
@@ -2,11 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField
 from wtforms import StringField
 from wtforms import SubmitField
-from wtforms.validators import length
+from wtforms.validators import length, EqualTo
 
 class RegisterForm(FlaskForm):
     username = StringField(label='User Name', validators=length(min=5, max=20))
     email_address = StringField(label='Email Address')
     password1 = PasswordField(label='Password', validators=length(min=8))
-    password2 = PasswordField(label='Confirm Password')
+    password2 = PasswordField(label='Confirm Password', validators=EqualTo('password1'))
     submit = SubmitField(label='Create Account')
