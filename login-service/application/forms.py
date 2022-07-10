32e908b4951f436d80508ab1e8a574aed7bc1da2
Kelvinskell <kelvinskelll@gmail.com>
2022-07-10 07:07:17 +0100

    feat: flash error messages

--- login-service/application/forms.py
+++ login-service/application/forms.py
@@ -2,11 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField
 from wtforms import StringField
 from wtforms import SubmitField
-from wtforms.validators import length, EqualTo
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name', validators=length(min=5, max=20))
-    email_address = StringField(label='Email Address')
-    password1 = PasswordField(label='Password', validators=length(min=8))
-    password2 = PasswordField(label='Confirm Password', validators=EqualTo('password1'))
+    username = StringField(label='User Name', validators=[Length(min=5, max=20), DataRequired()])
+    email_address = StringField(label='Email Address', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password', validators=[Length(min=8), DataRequired()])
+    password2 = PasswordField(label='Confirm Password', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
