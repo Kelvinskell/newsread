4235bd04c6ce02b3ee9300f0a12f3ad5fc0b0565
Kelvinskell <kelvinskelll@gmail.com>
2022-07-10 06:03:20 +0100

    feat: Add length validations in registration form

--- login-service/application/forms.py
+++ login-service/application/forms.py
@@ -2,10 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField
 from wtforms import StringField
 from wtforms import SubmitField
+from wtforms.validators import length
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name')
+    username = StringField(label='User Name', validators=length(min=5, max=20))
     email_address = StringField(label='Email Address')
-    password1 = PasswordField(label='Password')
+    password1 = PasswordField(label='Password', validators=length(min=8))
     password2 = PasswordField(label='Confirm Password')
     submit = SubmitField(label='Create Account')
