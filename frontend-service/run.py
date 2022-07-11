697fcca95bea726ffee0e6d96537257f727735ee
Kelvinskell <kelvinskelll@gmail.com>
2022-07-11 13:00:03 +0100

    feat: add resource blueprint

--- frontend-service/run.py
+++ frontend-service/run.py
@@ -1,4 +1,10 @@
 from application import app
+from application.continuous_resource_blueprint import blueprint as continuous_resource_blueprint
+from flask import Flask, jsonify
+
+def create_application() -> Flask:
+    app.register_blueprint(continuous_resource_blueprint)
+    return app
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
--- news-service/run.py
+++ news-service/run.py
@@ -1,4 +1,11 @@
 from application import app
+from application.continuous_resource_blueprint import blueprint as continuous_resource_blueprint
+from flask import Flask, jsonify
+
+def create_application() -> Flask:
+    app.register_blueprint(continuous_resource_blueprint)
+    return app
+
 
 if __name__ == '__main__':
     app.run(port=5002, debug=True)
