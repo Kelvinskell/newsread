31ded0df84eec7a7d0409c0f62c41ab14ec40e79
Kelvinskell <kelvinskelll@gmail.com>
2022-07-12 07:31:36 +0100

    feat: Remove sesitive data

--- application/__init__.py
+++ application/__init__.py
@@ -3,13 +3,16 @@ from flask import render_template
 import pymysql
 pymysql.install_as_MySQLdb()
 from flask_mysqldb import MySQL
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '08dae760c2488d8a0dca1bfb'
 app.config['MYSQL_DB'] = 'heroku_f0275f370907b6e'
 app.config['MYSQL_USER'] = 'b983b41c79227f'
 app.config['MYSQL_HOST'] = 'us-cdbr-east-06.cleardb.net'
-app.config['MYSQL_PASSWORD'] = '0a7f7397'
+app.config['MYSQL_PASSWORD'] = os.getenv('DATABASE_PASSWORD')
 mysql = MySQL(app)
 
 
