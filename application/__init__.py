b121abaa880aae078a04440d595ac469b2c1d535
Kelvinskell <kelvinskelll@gmail.com>
2022-07-12 06:55:53 +0100

    feat: Modify MYSQL config variables

--- application/__init__.py
+++ application/__init__.py
@@ -6,10 +6,10 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '08dae760c2488d8a0dca1bfb'
-app.config['MYSQL_DATABASE_USER'] = 'b983b41c79227f'
-app.config['MYSQL_DATABASE_PASSWORD'] = '0a7f7397'
-app.config['MYSQL_DATABASE_DB'] = 'heroku_f0275f370907b6e'
-app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-06.cleardb.net'
+app.config['MYSQL_DB'] = 'heroku_f0275f370907b6e'
+app.config['MYSQL_USER'] = 'b983b41c79227f'
+app.config['MYSQL_HOST'] = 'us-cdbr-east-06.cleardb.net'
+app.config['MYSQL_PASSWORD'] = '0a7f7397'
 mysql = MySQL(app)
 
 
