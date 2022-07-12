c5a4b5a4f69f9a31e033eb8d9da040bd2571ee44
Kelvinskell <kelvinskelll@gmail.com>
2022-07-12 05:39:51 +0100

    feat: Change database details

--- application/__init__.py
+++ application/__init__.py
@@ -9,9 +9,10 @@ database.create_table()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '08dae760c2488d8a0dca1bfb'
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_DB'] = 'newsread'
+app.config['MYSQL_DATABASE_USER'] = 'b983b41c79227f'
+app.config['MYSQL_DATABASE_PASSWORD'] = '0a7f7397'
+app.config['MYSQL_DATABASE_DB'] = 'heroku_f0275f370907b6e'
+app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-06.cleardb.net'
 mysql = MySQL(app)
 
 
