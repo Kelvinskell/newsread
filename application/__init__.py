49f7ca35166b6e7332dfe5fc3e1f4e4441abd10f
Kelvinskell <kelvinskelll@gmail.com>
2022-08-21 04:50:21 +0100

    mend

--- application/__init__.py
+++ application/__init__.py
@@ -9,16 +9,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '08dae760c2488d8a0dca1bfb'
-# app.config['MYSQL_DB'] = 'heroku_f0275f370907b6e'
-# app.config['MYSQL_USER'] = 'b983b41c79227f'
-# app.config['MYSQL_HOST'] = 'us-cdbr-east-06.cleardb.net'
-# app.config['MYSQL_PASSWORD'] = os.getenv('DATABASE_PASSWORD')
-app.config['MYSQL_DB'] = 'newsread'
-app.config['MYSQL_USER'] =  'root'
-app.config['MYSQL_HOST'] = 'mysqldb'
-app.config['MYSQL_PASSWORD'] = 'secret'
-app.config['MYSQL_ROOT_PASSWORD'] = 'secret'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_PASSWORD'] = os.getenv('DATABASE_PASSWORD')
+app.config['MYSQL_ROOT_PASSWORD'] = os.getenv('MYSQL_ROOT_PASSWORD')
 
 mysql = MySQL(app)
 
