ea7d1e9614023a53c8635213a6e1a3d810f246a6
Kelvinskell <kelvinskelll@gmail.com>
2022-08-21 04:49:33 +0100

    feat: Make DB values flexible

--- application/__init__.py
+++ application/__init__.py
@@ -1,3 +1,4 @@
+import secrets
 from flask import Flask
 from flask import render_template
 import pymysql
@@ -9,10 +10,16 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '08dae760c2488d8a0dca1bfb'
-app.config['MYSQL_DB'] = 'heroku_f0275f370907b6e'
-app.config['MYSQL_USER'] = 'b983b41c79227f'
-app.config['MYSQL_HOST'] = 'us-cdbr-east-06.cleardb.net'
-app.config['MYSQL_PASSWORD'] = os.getenv('DATABASE_PASSWORD')
+# app.config['MYSQL_DB'] = 'heroku_f0275f370907b6e'
+# app.config['MYSQL_USER'] = 'b983b41c79227f'
+# app.config['MYSQL_HOST'] = 'us-cdbr-east-06.cleardb.net'
+# app.config['MYSQL_PASSWORD'] = os.getenv('DATABASE_PASSWORD')
+app.config['MYSQL_DB'] = 'newsread'
+app.config['MYSQL_USER'] =  'root'
+app.config['MYSQL_HOST'] = 'mysqldb'
+app.config['MYSQL_PASSWORD'] = 'secret'
+app.config['MYSQL_ROOT_PASSWORD'] = 'secret'
+
 mysql = MySQL(app)
 
 
