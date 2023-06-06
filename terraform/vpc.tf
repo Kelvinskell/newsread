b893dfb2b7c60b43ad86fe37b76d999e156388a4
Kelvinskell <kelvinskelll@gmail.com>
2023-06-06 01:04:20 +0100

    Disable NAT gateway in VPC config

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,9 +8,7 @@ module "vpc" {
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
-  one_nat_gateway_per_az = false
+  enable_nat_gateway     = false
   enable_vpn_gateway     = false
   enable_dns_hostnames   = true
   enable_dns_support     = true
